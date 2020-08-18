cab319a8963ba2a79bf7b00ec97773c73e7b50a1
yiweng <yiweng.chow@ufinity.com>
2020-08-18 17:43:15 +0800

    Added appId

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'myAppId',
+  appId: process.env.APP_ID || '6Ws4a9aZkXDiUpjj2jjzVac33C7v0rpETOdqFM9t',
   masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
